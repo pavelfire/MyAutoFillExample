23d19e2ed547870f74170e980de6f43a5735ed1c
Pavel Do <fire_ra@bk.ru>
2024-01-28 22:23:55 +0300

    ready

--- app/src/main/java/com/vk/directop/myautofillexample/MainActivity.kt
+++ app/src/main/java/com/vk/directop/myautofillexample/MainActivity.kt
@@ -1,11 +1,78 @@
 package com.vk.directop.myautofillexample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.phone.SmsRetriever
+import com.vk.directop.myautofillexample.SmsBroadcastReceiver.SmsBroadcastReceiverListener
+import com.vk.directop.myautofillexample.databinding.ActivityMainBinding
+import java.util.regex.Pattern
+
+const val REQ_USER_CONSENT = 200
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var smsBroadcastReceiver: SmsBroadcastReceiver
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        startSmartUserConsent()
+    }
+
+    private fun startSmartUserConsent() {
+        val client = SmsRetriever.getClient(this)
+        client.startSmsUserConsent(null)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQ_USER_CONSENT) {
+            if (resultCode == RESULT_OK && data != null) {
+                val message = data.getStringExtra(SmsRetriever.EXTRA_SMS_MESSAGE)
+                getCodeFromMessage(message)
+            }
+        }
+    }
+
+    private fun getCodeFromMessage(message: String?) {
+        val codePattern = Pattern.compile("(|^)\\d{6}")
+        val matcher = codePattern.matcher(message)
+        if (matcher.find()) {
+            binding.et.setText(matcher.group(0))
+        }
+    }
+
+    private fun registerBroadcastReceiver() {
+        smsBroadcastReceiver = SmsBroadcastReceiver()
+        smsBroadcastReceiver.smsBroadcastReceiverListener = object : SmsBroadcastReceiverListener {
+            override fun onSuccess(intent: Intent) {
+                startActivityForResult(intent, REQ_USER_CONSENT)
+            }
+
+            override fun onFailure() {}
+        }
+        val intentFilter = IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            registerReceiver(smsBroadcastReceiver, intentFilter, RECEIVER_EXPORTED)
+        }else {
+            registerReceiver(smsBroadcastReceiver, intentFilter)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerBroadcastReceiver()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(smsBroadcastReceiver)
     }
 }
---
+++ app/src/main/java/com/vk/directop/myautofillexample/MainJavaActivity.java
@@ -0,0 +1,85 @@
+package com.vk.directop.myautofillexample;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.phone.SmsRetriever;
+import com.google.android.gms.auth.api.phone.SmsRetrieverClient;
+import com.vk.directop.myautofillexample.databinding.ActivityMainBinding;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainJavaActivity extends AppCompatActivity {
+
+    private static final int REQ_USER_CONSENT = 200;
+    SmsBroadcastReceiver smsBroadcastReceiver;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        startSmartUserConsent();
+    }
+
+    private void startSmartUserConsent() {
+        SmsRetrieverClient client = SmsRetriever.getClient(this);
+        client.startSmsUserConsent(null);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQ_USER_CONSENT) {
+            if ((resultCode == RESULT_OK) && (data != null)) {
+                String message = data.getStringExtra(SmsRetriever.EXTRA_SMS_MESSAGE);
+                getCodeFromMessage(message);
+            }
+        }
+    }
+
+    private void getCodeFromMessage(String message) {
+        Pattern codePattern = Pattern.compile("(|^)\\d{6}");
+        Matcher matcher = codePattern.matcher(message);
+        if (matcher.find()){
+            binding.et.setText(matcher.group(0));
+        }
+    }
+
+    private void registerBroadcastReceiver() {
+        smsBroadcastReceiver = new SmsBroadcastReceiver();
+        smsBroadcastReceiver.smsBroadcastReceiverListener = new SmsBroadcastReceiver.SmsBroadcastReceiverListener() {
+            @Override
+            public void onSuccess(@NonNull Intent intent) {
+                startActivityForResult(intent, REQ_USER_CONSENT);
+            }
+
+            @Override
+            public void onFailure() {
+
+            }
+        };
+
+        IntentFilter intentFilter = new IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION);
+        registerReceiver(smsBroadcastReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerBroadcastReceiver();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(smsBroadcastReceiver);
+    }
+}
